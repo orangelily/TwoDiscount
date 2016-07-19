ab4df17c8ffb7a0277cebe640b7cf388c77bd0a3
orangelily <547207352@qq.com>
2016-07-19 11:56:23 +0800

    多个不同商品结算

--- src/test/java/com/model/ParseInputTest.java
+++ src/test/java/com/model/ParseInputTest.java
@@ -1,12 +1,8 @@
 package com.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.exception.ProductBuyInfoException;
 import com.process.ParseInput;
 import com.process.ProcessDetail;
 
@@ -16,32 +12,40 @@ public class ParseInputTest {
 
 	@Test
 	public void json_parse_to_product_with_one_item_no_discount() throws Exception {
-		//单个商品结算
+		// 单个商品结算
 		String infoProductBuy = "['ITEM000001']";
 		ParseInput parseInput = new ParseInput();
 		productBuy = parseInput.parseInput(infoProductBuy);
 		ProcessDetail processDetail = new ProcessDetail();
 		String printInfo = processDetail.calculatebuy(productBuy);
-		
+
 		Assert.assertNotNull(printInfo);
 		System.out.println(printInfo);
 	}
 
 	@Test
 	public void json_parse_to_product_with_list_same_item_no_count() throws Exception {
-		//多个相同商品结算
+		// 多个相同商品结算
 		String infoProductBuy = "['ITEM000001','ITEM000001', 'ITEM000001']";
 		ParseInput parseInput = new ParseInput();
 		productBuy = parseInput.parseInput(infoProductBuy);
 		ProcessDetail processDetail = new ProcessDetail();
 		String printInfo = processDetail.calculatebuy(productBuy);
-		
+
 		Assert.assertNotNull(printInfo);
 		System.out.println(printInfo);
 	}
-	
+
 	@Test
-	public void json_parse_to_product_with_list_same_item_and_count() throws Exception {
-		
+	public void json_parse_to_product_with_list_diff_item_no_count() throws Exception {
+		// 多个不同商品结算
+		String infoProductBuy = "['ITEM000001','ITEM000002', 'ITEM000000', 'ITEM000000']";
+		ParseInput parseInput = new ParseInput();
+		productBuy = parseInput.parseInput(infoProductBuy);
+		ProcessDetail processDetail = new ProcessDetail();
+		String printInfo = processDetail.calculatebuy(productBuy);
+
+		Assert.assertNotNull(printInfo);
+		System.out.println(printInfo);
 	}
 }
