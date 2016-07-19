60ee870b4dd4dca46f354272710e94f0a6d49af4
orangelily <547207352@qq.com>
2016-07-19 11:02:04 +0800

    添加商品条形码输入异常处理类

---
+++ src/main/java/com/exception/AddProductBuyInfoException.java
@@ -0,0 +1,12 @@
+package com.exception;
+
+/**
+ * 	收银机扫码添加到待打印列表异常处理
+ * @author kl
+ *
+ */
+public class AddProductBuyInfoException extends Exception{
+	public AddProductBuyInfoException() {
+		super();
+	}
+}
